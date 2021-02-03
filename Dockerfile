b3d16d39c2ae64b1ccd85e9485b6e1c23c9504e2
Aurel Landry KENGNI NKONGTCHOU <lkengni@tagerim.fr>
2021-02-03 18:37:12 +0100

    Initial Commit

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM python:3.8-alpine
+
+ENV PATH="/scripts:${PATH}"
+
+COPY ./requirements.txt /requirements.txt
+RUN apk add --update --no-cache --virtual .tmp gcc libc-dev linux-headers
+RUN pip install -r /requirements.txt
+RUN apk del .tmp
+
+RUN mkdir /test_project
+COPY ./app /app
+WORKDIR /app
+COPY ./scripts /scripts
+
+RUN chmod +x /scripts/*
+
+RUN adduser -D user
+USER user
---
+++ app/users/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
+from django.http import JsonResponse
+
+
+""""
+Affichage de la page 'Profil' qui recense toutes les
+informations utilisateur
+"""
+@login_required
+def profil(request):
+    return render(request, 'profil/profil.html')
+
+"""
+Modification des informations utilisateur
+"""
+def updateUser(request):
+    user = request.user
+
+    if request.method == "POST":
+        user.email  = request.POST["email"]
+        user.save()
+
+        return JsonResponse({'email': user.email}, status=200)
+    else:
+        return JsonResponse(
+            {
+                "message": "Method Not Allowed"
+            },
+            status=405
+        )
